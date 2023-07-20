ad7597e5954d3e14a61221519f3f7363f1cc08fe
MichaelKerr0702 <110789076+MichaelKerr0702@users.noreply.github.com>
2023-07-19 17:11:41 -0700

    new save for contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Col, Row } from "react-bootstrap";
+import { Button, Col, Row } from "react-bootstrap";
 import contactImg from '../assets/img/contact-img.svg'
 
 export const Contact = () => {
@@ -33,14 +33,28 @@ export const Contact = () => {
                     <form>
                         <Row>
                             <Col sm = {6} className='px-1'>
-                                <input type='text' value={formDetails.firstName} placeholder='First Name' onChange={(e)=> onFormUpdate('firstName', e.target.value)}
+                                <input type='text' value={formDetails.firstName} placeholder='First Name' onChange={(e)=> onFormUpdate('firstName', e.target.value)}/>
                             </Col>
                             <Col sm = {6} className='px-1'>
-
+                                <input type='text' value={formDetails.lastName} placeholder='Last Name' onChange={(e)=> onFormUpdate('lastName', e.target.value)}/>
                             </Col>
                             <Col sm = {6} className='px-1'>
-
+                                <input type='email' value={formDetails.email} placeholder='Email Adress' onChange={(e)=> onFormUpdate('email', e.target.value)}/>
                             </Col>
+                            <Col sm = {6} className='px-1'>
+                                <input type='tel' value={formDetails.phone} placeholder='Phone No.' onChange={(e)=> onFormUpdate('phone', e.target.value)}/>
+                            </Col>
+                            <Col sm = {6} className='px-1'>
+                                <textarea row = '6' value={formDetails.message} placeholder='Message' onChange={(e) => onFormUpdate('message', e.target.value)}/>
+                                <button type="submit"><span>{buttonText}</span></button>
+                            </Col>
+                            {
+                                status.message &&
+                                <Col>
+                                    <p className={status.success=}
+                                </Col>
+                            }
+
                         </Row>
                     </form>
                 </Col>
